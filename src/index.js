3b1370bc1ed1cefa3019c0156e9e13ce5a9cddf7
Erwin Kodiat <750222+erwin-kodiat@users.noreply.github.com>
2023-12-03 20:05:25 +0700

    Deployed

--- src/index.js
+++ src/index.js
@@ -4,23 +4,21 @@ import userJwt1 from "./budi.js";
 export default {
 	async fetch(request, env, ctx) {
 
-		//let userJwt = request.headers.get("Cf-Access-Jwt-Assertion");
-		//console.log(userJwt.name);
-		let userJwt = userJwt1.name;
+		let userJwt = request.headers.get("Cf-Access-Jwt-Assertion") || userJwt1.name;
 
 		const { payload } = jwt.decode(userJwt);
 		const dateTimestamp  = (new Date(payload.iat * 1000)).toISOString();
 		const url = "https://restcountries.com/v3.1/alpha/" + payload.country;
-		console.log('14');
-    const response = await fetch(url, {
+
+		const response = await fetch(url, {
 			headers: {
         "content-type": "application/json;charset=UTF-8",
       },
 		});
-		console.log('20');
+
 		const namanya = await response.json();
 		const countryName = namanya[0].name.common;
-		console.log("countryName: " + countryName);
+
 		const strCountry = "<a href=\"/secure/" + payload.country + "\" class=\"text-blue-400 hover:underline\">"+ countryName +"</a>";
 
 		var html = "<!DOCTYPE html>";
