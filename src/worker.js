bd2347a6db18a99c25367decafb46d4c24cf16e2
Erwin Kodiat <750222+erwin-kodiat@users.noreply.github.com>
2023-12-03 11:14:50 +0700

    JWT implementation

--- src/worker.js
+++ src/worker.js
@@ -7,9 +7,28 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import jwt from '@tsndr/cloudflare-worker-jwt';
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		var txt = 'Hello World!' + "\n";
+		// console.log(request.headers);
+		// console.log("Kok sepi?");
+
+		// let userEmail = request.headers.get("Cf-Access-Authenticated-User-Email") || '_development';
+    // txt += userEmail + "\n";
+
+		let userJwt = request.headers.get("Cf-Access-Jwt-Assertion") || '_production';
+    // txt += userJwt + "\n";;
+
+		const { payload } = jwt.decode(userJwt);
+		txt += "Email: " + payload.email + "\n";
+		txt += "Timestamp: " + payload.iat + "\n";
+		txt += "Country: " + payload.country + "\n";
+
+		// console.log(payload.aud);
+
+		return new Response(txt);
 	},
 };
+
